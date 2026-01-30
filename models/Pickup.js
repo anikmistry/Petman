f533d036f44d9c969c50a02db66b287aafad9100
anikmistry <anikmistry8963@gmail.com>
2026-01-31 01:20:32 +0600

    feat: Implement user and admin dashboards, pickup management, and authentication with new models, routes, and middleware.

--- models/Pickup.js
+++ models/Pickup.js
@@ -2,10 +2,13 @@ const mongoose = require('mongoose');
 
 const PickupSchema = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  wasteType: [{ type: String, required: true }],
+  wasteType: [{ 
+    type: String, 
+    enum: ['plastic', 'paper', 'electronics', 'used_clothing', 'metal', 'cardboard'],
+    required: true }],
   quantity: { type: Number, required: true },
   details: { type: String },
-  scheduledFor: { type: Date, default: Date.now },
+  scheduledFor: { type: Date, default: Date.now, required: true },
   status: { type: String, enum: ['pending','scheduled','completed','cancelled'], default: 'pending' },
 }, { timestamps: true });
 
